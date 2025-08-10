b412473342eac5e426af8a549a5383f886c8ff52
Jcanotorr06 <jcano062001@gmail.com>
2025-08-10 10:48:13 -0500

    Add authentication configuration using MSAL for Azure

---
+++ client/src/config/authConfig.ts
@@ -0,0 +1,44 @@
+import { type Configuration, LogLevel } from "@azure/msal-browser";
+
+const msalConfig = {
+  auth: {
+    clientId: import.meta.env.VITE_CLIENT_ID,
+    authority: import.meta.env.VITE_CLIENT_AUTHORITY,
+    redirectUri: import.meta.env.VITE_REDIRECT_URI,
+    postLogoutRedirectUri: import.meta.env.VITE_LOGOUT_URI,
+    navigateToLoginRequestUrl: false,
+  },
+  cache: {
+    cacheLocation: "sessionStorage",
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: (level, message, containsPii) => {
+        if (containsPii) return;
+
+        switch (level) {
+          case LogLevel.Error:
+            console.error(message);
+            break;
+          case LogLevel.Info:
+            console.info(message);
+            break;
+          case LogLevel.Verbose:
+            console.debug(message);
+            break;
+          case LogLevel.Warning:
+            console.warn(message);
+            break;
+          default:
+            console.log(message);
+        }
+      },
+    },
+  },
+} satisfies Configuration;
+
+const loginRequest = {
+  scopes: [],
+};
+
+export { msalConfig, loginRequest };
