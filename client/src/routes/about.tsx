21be7e7e5657701f8897395a7138bcd2ef22836b
Jcanotorr06 <jcano062001@gmail.com>
2025-08-11 00:15:32 -0500

    Add loader function to About route for breadcrumb support

--- client/src/routes/about.tsx
+++ client/src/routes/about.tsx
@@ -2,6 +2,11 @@ import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/about")({
   component: About,
+  loader: () => {
+    return {
+      crumb: "About",
+    };
+  },
 });
 
 function About() {
