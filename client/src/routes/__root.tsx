fc4181bc615515125409a4d0c3c2502652c77bca
Jcanotorr06 <jcano062001@gmail.com>
2025-08-11 00:14:52 -0500

    Refactor root route to use context and improve loading state handling

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,24 +1,49 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { SidebarInset, SidebarProvider } from "@/components/molecules";
+import { AppNavigation, AppSidebar } from "@/components/organisms";
+import type { useToken } from "@/context";
+import { getListsQueryOptions, getPublicListsQueryOptions } from "@/services";
+import type { QueryClient } from "@tanstack/react-query";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { LoaderCircle } from "lucide-react";
 
-export const Route = createRootRoute({
+type RootRouteContext = {
+  queryClient: QueryClient;
+  tokenContext: ReturnType<typeof useToken>;
+};
+
+export const Route = createRootRouteWithContext<RootRouteContext>()({
   component: RootLayout,
+  pendingComponent: () => (
+    <div className="w-screen h-screen flex items-center justify-center bg-background">
+      <div className="flex flex-row items-center justify-center gap-4 w-fit flex-wrap">
+        <LoaderCircle className="w-12 h-12 animate-spin" />
+        <h3 className="text-2xl font-semibold">Loading...</h3>
+      </div>
+    </div>
+  ),
+  loader: ({ context }) => {
+    const { queryClient, tokenContext } = context;
+    if (tokenContext.headers.has("Authorization")) {
+      queryClient.ensureQueryData(getListsQueryOptions(tokenContext.headers));
+      queryClient.ensureQueryData(
+        getPublicListsQueryOptions(tokenContext.headers)
+      );
+    }
+  },
 });
 
 function RootLayout() {
   return (
-    <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
-      <hr />
-      <Outlet />
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <AppNavigation />
+        <div className="flex flex-1 flex-col gap-4 pt-0">
+          <Outlet />
+        </div>
+      </SidebarInset>
       {import.meta.env.DEV ? <TanStackRouterDevtools /> : null}
-    </>
+    </SidebarProvider>
   );
 }
